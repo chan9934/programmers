7d1273ae679a9b410050b973ecba7d6dd6360b0b
chan9934 <kim75249934@gmail.com>
2022-07-02 11:19:22 +0900

    0702

--- 핸드폰 번호 가리기.cpp
+++ 핸드폰 번호 가리기.cpp
@@ -24,28 +24,28 @@ string solution(string phone_number)
   else if (phone_number.length() == 5)
   {
 
-      answer = "*" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+      answer = "*" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
       return answer;
       cout << answer;
   }
      else if (phone_number.length() == 6)
     {
 
-    answer = "**" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+    answer = "**" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
     return answer;
     cout << answer;
     }
    else if (phone_number.length() == 7)
     {
 
-        answer = "***" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "***" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else  if (phone_number.length() == 8)
     {
 
-        answer = "****" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "****" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
@@ -54,75 +54,75 @@ string solution(string phone_number)
     else if (phone_number.length() == 9)
     {
 
-        answer = "*****" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "*****" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
 
     else if (phone_number.length() == 10)
     {
-        answer = "******" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "******" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 11)
     {
-        answer = "*******" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "*******" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 12)
     {
-        answer = "********" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "********" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 13)
     {
-        answer = "*********" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "*********" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 14)
     {
        
-        answer = "**********" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "**********" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 15)
     {
-        answer = "***********" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "***********" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 16)
     {
-        answer = "************" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "************" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 17)
     {
-        answer = "*************" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "*************" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 18)
     {
-        answer = "**************" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "**************" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 19)
     {
-        answer = "***************" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "***************" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
     else if (phone_number.length() == 20)
     {
-        answer = "****************" + phone_number.substr((phone_number.length() - 4), (phone_number.length()));
+        answer = "****************" + phone_number.erase((phone_number.length() - 4), (phone_number.length()));
         return answer;
         cout << answer;
     }
