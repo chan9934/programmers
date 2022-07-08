df648c5af613345464280d07286a274f014e62b9
chan9934 <kim75249934@gmail.com>
2022-07-08 09:00:58 +0900

    0708

--- 숫자 문자열과 영단어.cpp
+++ 숫자 문자열과 영단어.cpp
@@ -30,42 +30,104 @@ using std::cin;
 using std::endl;
 using std::string;
 
-enum Number
+//#define "zero" = '1';
+//
+//
+//enum Number
+//{
+//	"zero" = '1',
+//	one	,	
+//	two	,
+//	three,
+//	four,
+//	five,
+//	six	,
+//	seven,
+//	eight,
+//	nine	
+//};
+
+string substitute(string a)
 {
-	zero,	
-	one	,	
-	two		,
-	three	,
-	four	,
-	five	,
-	six		,
-	seven	,
-	eight	,
-	nine	
-};
+	if ("zero" == a)
+	{
+		a = '0';
+	}
+	else if ("one" == a)
+	{
+		a = '1';
+	}
+	else if ("two" == a)
+	{
+		a = '2';
+	}
+	else if ("three" == a)
+	{
+		a = '3';
+	}
+	else if ("four" == a)
+	{
+		a = '4';
+	}
+	else if ("five" == a)
+	{
+		a = '5';
+	}
+	else if ("six" == a)
+	{
+		a = '6';
+	}
+	else if ("seven" == a)
+	{
+		a = '7';
+	}
+	else if ("eight" == a)
+	{
+		a = '8';
+	}
+	else if ("nine" == a)
+	{
+		a = '9';
+	}
+
+	else
+	{
+		return a;
+	}
+
+	return a;
+}
 
 int solution(string a)
 {
+
 	int answer = 0;
-	return answer;
+	
+	string result = "";
+	string b = "";
 
 	for (int i = 0; i < a.size(); ++i)
 	{
+		
 		if (65 <= a[i])
-		{
-			for (int j = i; j < a.size(); ++j)
+		{	
+			b.push_back(a[i]);
+			if (b != substitute(b))
 			{
-				if (a[j] < 65)
-				{
-					a.substr(i, j-1)
-					break;
-
-				}
-
-				
+				result = result + substitute(b);
+				b = "";
 			}
-
 		}
+		else
+			result.push_back(a[i]);
 	}
+	answer = stoi(result);
+	return answer;
 
+}
+
+int main()
+{
+	string a = "one2three";
+	cout << solution(a);
 }
